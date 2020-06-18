6cf4be98f20fba146d78b49ccc6783edb1d7b41b
kpose <ganihujude@gmail.com>
2020-06-18 11:15:07 +0100

    setup dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
 module.exports = {
-    module : {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node.modules/,
-                use: {
-                    loader:'"babel-loader',
-                },
-            },
-        ],
-    },
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader:'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
