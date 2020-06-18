e24bf5b967d80bebda6bf3d0377f0f7ab0775afc
kpose <ganihujude@gmail.com>
2020-06-18 11:26:52 +0100

    structure project

---
+++ src/containers/List.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Card from '../components/Card';
+
+export default class List extends Component {
+  render() {
+    return (
+      <Card />
+    )
+  }
+}
